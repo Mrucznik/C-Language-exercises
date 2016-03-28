ec489023b543ba52e566eb00263597d399a57161
Szymon Gajda <mrucznix@gmail.com>
2016-03-28 20:48:00 +0200

    Exercise 7 done

---
+++ Semester II/zadanie7/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned int length(int tab[])
+{
+	int i = 0;
+	while (tab[i] != -1)
+		i++;
+	return i;
+}
+
+int main()
+{
+	int tab[] = { 16909060, 84281096, 151653132, -1 };
+	printf("Dlugosc: %d\n", length(tab));
+	return 0;
+}
