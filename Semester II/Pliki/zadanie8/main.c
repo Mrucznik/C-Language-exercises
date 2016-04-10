bb898b50dc1a0296847d8f8ff8cd808c3b3b3af0
Szymon Gajda <mrucznix@gmail.com>
2016-04-10 22:35:20 +0200

    Exercise 8

--- Semester II/Pliki/zadanie8/main.c
+++ Semester II/Pliki/zadanie8/main.c
@@ -2,6 +2,28 @@
 
 int main()
 {
+	char filename[FILENAME_MAX];
+	
+	printf("Podaj nazwe pliku do wczytania: ");
+	scanf("%s", filename);
+
+	FILE* f = fopen(filename, "rb");
+
+	if (f == NULL)
+	{
+		printf("Error! Nie mozna otworzyc pliku.");
+		return 1;
+	}
+
+	while (!feof(f))
+	{
+		char tmp=0;
+		fread(&tmp, sizeof(tmp), 1, f);
+		printf("%d ", tmp);
+	}
+	printf("\n");
+
+	fclose(f);
 	return 0;
 }
 
