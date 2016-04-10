2ce00c5705e89a4ca4afcfeacef7a8bbed3e8bc0
Szymon Gajda <mrucznix@gmail.com>
2016-04-10 22:22:35 +0200

    Exercise 6 and 7

--- Semester II/Pliki/zadanie6/main.c
+++ Semester II/Pliki/zadanie6/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main()
 {
+	srand(time(NULL));
+
+	FILE* f = fopen("file", "wb");
+	float tab[100];
+
+	for (int i = 0; i < 100; i++)
+	{
+		tab[i] = rand() % 201 - 100;
+	}
+
+	if (f == NULL)
+	{
+		printf("Error! Nie mozna otworzyc pliku.");
+		return 1;
+	}
+
+	fwrite(tab, sizeof(float), 100, f);
+
+	fclose(f);
 	return 0;
 }
 
--- Semester II/Pliki/zadanie7/main.c
+++ Semester II/Pliki/zadanie7/main.c
@@ -2,6 +2,24 @@
 
 int main()
 {
+
+	FILE* f = fopen("..\\zadanie6\\file", "rb");
+	float tab[100];
+
+	if (f == NULL)
+	{
+		printf("Error! Nie mozna otworzyc pliku.");
+		return 1;
+	}
+
+	fread(tab, sizeof(float), 100, f);
+
+	for (int i = 0; i < 100; i++)
+	{
+		printf("%f\n", tab[i]);
+	}
+
+	fclose(f);
 	return 0;
 }
 
