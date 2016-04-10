5b6393e0ce4653a4d2c6216e5d9915641d0e7cdc
Szymon Gajda <mrucznix@gmail.com>
2016-04-10 22:58:46 +0200

    Exercise 9a, 9b

--- Semester II/Pliki/zadanie9b/main.c
+++ Semester II/Pliki/zadanie9b/main.c
@@ -2,6 +2,25 @@
 
 int main()
 {
+	FILE* f = fopen("..\\zadanie9a\\file.txt", "r");
+	int ile = 0;
+
+	if (f == NULL)
+	{
+		printf("Error! Nie mozna otworzyc pliku.");
+		return 1;
+	}
+
+	while (!feof(f))
+	{
+		char string[100];
+		fgets(string, 100, f);
+		printf("%s", string);
+		ile++;
+	}
+	printf("Wyswietlono %d rekordow\n", ile);
+
+	fclose(f);
 	return 0;
 }
 
