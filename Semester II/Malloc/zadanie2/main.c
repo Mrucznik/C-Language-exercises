dcc386ef255db13181480c33b41c43cf2c24f893
Szymon Gajda <mrucznix@gmail.com>
2016-04-23 21:41:31 +0200

    Exercise 2

---
+++ Semester II/Malloc/zadanie2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char * byte = (char*)malloc(1024 * 1024 * 1024);
+	if (byte == NULL) printf("Nie udalo sie zaalokowac pamieci!");
+	getchar();
+	return 0;
+}
