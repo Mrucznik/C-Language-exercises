fd6864bd0d850fd0e43cdde567e6a10599f0218c
Szymon Gajda <mrucznix@gmail.com>
2016-05-20 18:31:22 +0200

    Poprawki

--- Semester II/Malloc2/zadanie3/funkcje_geom.h
+++ Semester II/Malloc2/zadanie3/funkcje_geom.h
@@ -23,11 +23,11 @@ struct fgeom_t
 
 typedef struct fgeom_t fgeom_t;
 
-fgeom_t* generuj_trojkat(float, float, float, float, float, float, int*);
-fgeom_t* generuj_kolo(float, float, float, int*);
-fgeom_t* generuj_kwadrat(float, float, float, float, float, float, float, float, int*);
-fgeom_t* generuj_odcin(float, float, float, float, int*);
-void wyswietl_figure(fgeom_t);
-void zapisz_figure(fgeom_t**, int);
-void wczytaj_figure(fgeom_t**, int);
+extern fgeom_t* generuj_trojkat(float, float, float, float, float, float, int*);
+extern fgeom_t* generuj_kolo(float, float, float, int*);
+extern fgeom_t* generuj_kwadrat(float, float, float, float, float, float, float, float, int*);
+extern fgeom_t* generuj_odcin(float, float, float, float, int*);
+extern void wyswietl_figure(fgeom_t);
+extern void zapisz_figure(fgeom_t**, int);
+extern void wczytaj_figure(fgeom_t**, int);
 
